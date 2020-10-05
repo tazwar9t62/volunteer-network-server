c846c60941acaf806c250c90ea48b1af5f2ae7a6
Tazwar <tazwar9t6@gmail.com>
2020-10-05 14:58:16 +0600

    added registration

--- index.js
+++ index.js
@@ -1,11 +1,56 @@
 const express = require("express");
-const app = express();
-const port = 5000;
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.send("Assignmnet 10 , Bismillah");
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const uri =
+  "mongodb+srv://9t6:livelovedie@cluster0.dupvj.mongodb.net/volunteer-network?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+const port = 5000;
+
+client.connect((err) => {
+  const tasks = client.db("volunteer-network").collection("tasks");
+
+  const registrations = client
+    .db("volunteer-network")
+    .collection("registrations");
+
+  app.get("/tasks", (req, res) => {
+    tasks.find({}).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+  app.post("/addRegistration", (req, res) => {
+    const activity = req.body;
+    registrations.insertOne(activity).then((result) => {
+      res.send(result);
+      console.log(result);
+    });
+  });
+
+  app.get("/activities", (req, res) => {
+    const activity = req.body;
+    activities.find({ email: req.query.email }).toArray((err, documents) => {
+      res.send(documents);
+    });
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || port, () => {
+  console.log("Listening port 5000");
 });
